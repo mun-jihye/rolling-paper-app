d6f1183237fc2a12f062df16fe0039c34883d7df
min3eo <kane7674@naver.comm>
2024-02-27 20:53:48 +0900

    [#3] feat: 헤더 스타일 일부 수정과  미디어 쿼리 적용

--- src/components/commons/header/MainHeader.jsx
+++ src/components/commons/header/MainHeader.jsx
@@ -1,53 +1,59 @@
 import styled from 'styled-components';
 import Logo from './Logo.svg';
 
-const StyledLogo = styled.img`
-  width: 10.6rem;
-  height: 4.2rem;
-  margin-left: 36rem;
-  padding-top: 1.1rem;
-`;
-
-const StyledButton = styled.button`
-  width: 16rem;
-  height: 4.2rem;
-  border: 0.1rem;
-  border-radius: 0.8rem;
-  font-family: Pretendard;
-  font-size: 1.4rem;
-  padding: 0.8rem 1.6rem 0.8rem 1.6rem;
-  border: 0.1rem solid black;
-  text-align: center;
-  margin-right: 36rem;
-  margin-top: 1rem;
-  background-color: ${({ theme }) => theme.white};
-`;
-
-const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const Divider = styled.div`
-  height: 0.05rem;
-  background-color: ${({ theme }) => theme.gray300}; // 테마의 gray300 색상 사용
-  margin: 1rem 0;
-`;
-
 const MainHeader = () => {
-  const handleClick = (e) => {
-    e.preventDefault()
-  }
+  // 롤링 페이퍼 만들기 버튼 이벤트
+  const handleClick = e => {
+    e.preventDefault();
+  };
+
   return (
     <>
-        <Container>
-          <StyledLogo src={Logo} alt="로고" />
-          <StyledButton onClick={handleClick}>롤링 페이퍼 만들기</StyledButton>
-        </Container>
-        <Divider />
+      <StyledContainer>
+        <StyledLogo src={Logo} alt="로고" />
+        <StyledButton onClick={handleClick}>롤링 페이퍼 만들기</StyledButton>
+      </StyledContainer>
+      <StyledDivider />
     </>
   );
 };
 
 export default MainHeader;
+
+const StyledLogo = styled.img`
+  width: 10.6rem;
+  height: 4.2rem;
+  padding-top: 1.2rem;
+`;
+
+const StyledButton = styled.button`
+  width: 16rem;
+  height: 4rem;
+  border: 0.1rem solid black;
+  border-radius: 1rem;
+  font-family: Pretendard;
+  font-size: 1.3rem;
+  padding: 0.3rem 1.4rem;
+  text-align: center;
+  background-color: ${({ theme }) => theme.white};
+  margin-top: 0.8rem;
+`;
+
+const StyledContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 24px;
+
+  @media (min-width: 1248px) {
+    padding: 0;
+    margin-left: 36rem;
+    margin-right: 36rem;
+  }
+`;
+
+const StyledDivider = styled.div`
+  height: 0.05rem;
+  background-color: ${({ theme }) => theme.gray300};
+  margin: 1rem 0;
+`;
