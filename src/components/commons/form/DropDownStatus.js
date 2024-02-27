28ec8244db46f518adfb77d8c9d0ef191135daf5
KimChangmin <zzangmin3@gmail.com>
2024-02-28 02:10:04 +0900

    cleanup TextFieldInput

--- src/components/commons/form/DropDownStatus.js
+++ src/components/commons/form/DropDownStatus.js
@@ -1,34 +1,33 @@
-export const DropDownStatus = (theme = '') => {
-  return {
-    inActive: {
-      color: `${({ theme }) => theme.gray300}`,
-      border: `1px solid ${({ theme }) => theme.gray300}`,
-      backgroundColor: `${theme.gray100}`,
-    },
-    focused: {
-      color: `${({ theme }) => theme.gray300}`,
-      border: `2px solid ${({ theme }) => theme.gray500}`,
-      backgroundColor: `${theme.gray100}`,
-    },
-    active: {
-      color: `${({ theme }) => theme.gray300}`,
-      border: `2px solid ${({ theme }) => theme.gray700}`,
-      backgroundColor: `${theme.gray100}`,
-    },
-    hover: {
-      color: `${({ theme }) => theme.gray300}`,
-      border: `1px solid ${({ theme }) => theme.gray500}`,
-      backgroundColor: `${theme.gray100}`,
-    },
-    disabled: {
-      color: `${({ theme }) => theme.gray300}`,
-      border: `1px solid ${({ theme }) => theme.gray300}`,
-      backgroundColor: `${theme.gray100}`,
-    },
-    error: {
-      color: `${({ theme }) => theme.gray300}`,
-      border: `1px solid ${({ theme }) => theme.error}`,
-      backgroundColor: `${theme.gray100}`,
-    },
-  };
+import { theme } from 'components/styles/theme';
+export const DropDownStatus = {
+  inActive: {
+    color: `${theme.gray300}`,
+    border: `1px solid ${theme.gray300}`,
+    backgroundColor: `#fff`,
+  },
+  focused: {
+    color: `${theme.gray300}`,
+    border: `2px solid ${theme.gray500}`,
+    backgroundColor: `#fff`,
+  },
+  active: {
+    color: `${theme.gray300}`,
+    border: `2px solid ${theme.gray700}`,
+    backgroundColor: `#fff`,
+  },
+  hover: {
+    color: `${theme.gray300}`,
+    border: `1px solid ${theme.gray500}`,
+    backgroundColor: `#fff`,
+  },
+  disabled: {
+    color: `${theme.gray400}`,
+    border: `1px solid ${theme.gray300}`,
+    backgroundColor: `${theme.gray100}`,
+  },
+  error: {
+    color: `${theme.gray900}`,
+    border: `1px solid ${theme.error}`,
+    backgroundColor: `#fff`,
+  },
 };
