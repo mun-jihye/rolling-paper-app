22738dbdbadfeac271ae0cadd66fc4c65427e5c3
mun-jihye <m84422583@gmail.com>
2024-02-27 14:34:19 +0900

    feat: 초기 라우트 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import routes from 'components/utils/constants/routes';
+import { Routes, Route } from 'react-router-dom';
+import ButtonPage from 'components/pages/ButtonPage';
+import FormPage from 'components/pages/FormPage';
+import Header from 'components/pages/Header';
+import Modal from 'components/pages/Modal';
+import CardListPage from 'components/pages/CardLIstPage';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path={routes.button} element={<ButtonPage />}></Route>
+      <Route path={routes.form} element={<FormPage />}></Route>
+      <Route path={routes.header} element={<Header />}></Route>
+      <Route path={routes.cardList} element={<CardListPage />}></Route>
+      <Route path={routes.modal} element={<Modal />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
