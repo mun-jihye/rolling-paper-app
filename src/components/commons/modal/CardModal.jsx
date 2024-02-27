fa6df227a3b6b44212baa857ec82d896cde8c90c
mun-jihye <m84422583@gmail.com>
2024-02-27 16:29:10 +0900

    [#5] feat: modal layout 잡기

---
+++ src/components/commons/modal/CardModal.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CardModal = () => {
+  return <div>CardModal</div>;
+};
+
+export default CardModal;
--- src/components/pages/ModalPage.jsx
+++ src/components/pages/ModalPage.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import Modal from 'components/commons/modal/Modal';
+import React, { useState } from 'react';
+import CardModal from 'components/commons/modal/CardModal';
 
 const ModalPage = () => {
-  return <div>Modal</div>;
+  const [showModal, setShowModal] = useState(true);
+  const handleClose = () => {
+    setShowModal(false);
+  };
+  return (
+    <>
+      <div>Modal</div>
+      {showModal && (
+        <Modal showModal={showModal} handleClose={handleClose}>
+          <CardModal />
+        </Modal>
+      )}
+    </>
+  );
 };
 
 export default ModalPage;
