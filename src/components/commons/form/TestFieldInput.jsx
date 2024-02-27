1d1a195fdecc6e0468e57979292c38db43763166
KimChangmin <zzangmin3@gmail.com>
2024-02-27 23:35:35 +0900

    test commit

--- src/components/commons/form/TestFieldInput.jsx
+++ src/components/commons/form/TestFieldInput.jsx
@@ -1,62 +1,30 @@
 import style from 'styled-components';
-import { useState } from 'react';
-import { theme } from 'components/styles/theme';
 
 const StyledDiv = style.div`
   display: flex;
   flex-direction: column;
   `;
 
-const styledThemes = {
-  inActive: {
-    color: theme.gray300,
-    borderColor: theme.gray300,
-    backgroundColor: theme.gray500,
-  },
-  focused: {
-    color: theme.gray900,
-    borderColor: theme.gray500,
-  },
-  active: {
-    color: theme.gray900,
-    borderColor: theme.gray700,
-  },
-  hover: {
-    color: theme.gray500,
-    borderColor: theme.gray500,
-  },
-  disabled: {
-    color: theme.gray400,
-    borderColor: theme.gray300,
-    backgroundColor: theme.gray100,
-  },
-  error: {
-    color: theme.gray900,
-    borderColor: theme.error,
-  },
-};
-
 const StyledInput = style.input`
-  border: 1px solid ${props => props.styledTheme.borderColor};
+  border: ${props => props.status.borderColor};
   border-radius: 0.8rem;
   padding: 1.2rem 1.6rem;
   height: 5rem;
   width: 32rem;
-  color: ${props => props.styledTheme.color};
-  background-color: ${props => props.styledTheme.backgroundColor};
+  color: ${props => props.status.color};
+  background-color: ${props => props.status.backgroundColor};
     
 `;
 
 const StyledSpan = style.span`
-  color: ${theme.error};
+  color: ${({ theme }) => theme.error};
 `;
 
-export function TextFieldInput() {
-  const [styledTheme, setStyledTheme] = useState(styledThemes.error);
+export function TextFieldInput({ status }) {
   return (
     <StyledDiv>
       Input
-      <StyledInput type="text" styledTheme={styledTheme} />
+      <StyledInput type="text" status={status} />
       <StyledSpan>message</StyledSpan>
     </StyledDiv>
   );
--- src/components/pages/FormPage.jsx
+++ src/components/pages/FormPage.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 
 const formPage = () => {
@@ -5,3 +6,31 @@ const formPage = () => {
 };
 
 export default formPage;
+=======
+import { React, useState } from 'react';
+import {
+  TextFieldInput,
+  TextFieldDropDown,
+  TextFieldTextEditor,
+  InputStatus,
+  DropDownStatus,
+} from 'components/commons/form';
+const handleInputChange = e => {
+  useState(InputStatus.focused);
+};
+
+const FormPage = () => {
+  //이벤트 핸들러를 통해 TestFieldInput의 상태를 변경할 수 있다.
+  const [status, setStatus] = useState(InputStatus.active);
+  const [status2, setStatus2] = useState(DropDownStatus.active);
+  return (
+    <div>
+      <TextFieldInput status={status} />
+      <TextFieldDropDown status={status2} />
+      <TextFieldTextEditor />
+    </div>
+  );
+};
+
+export default FormPage;
+>>>>>>> 4c4bff5 (test commit)
