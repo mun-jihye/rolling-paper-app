27e8dab0b4c5fadd5dd598996a939cce7b48ec5b
Jin-Chanyong <cksdyd324@gmail.com>
2024-02-29 00:15:18 +0900

    [#6] feat: 태블릿, 모바일 사이즈 드래그 캐러셀 기능 추가

--- src/components/commons/cardList/CardList.jsx
+++ src/components/commons/cardList/CardList.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import Card from './Card';
 import tempImg from 'components/assets/images/cardList/tempImg.jpg';
 import arrowImg from 'components/assets/images/cardList/arrow.png';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const mockData = {
   name: 'Chanyong',
@@ -16,12 +16,20 @@ const mockData = {
   ],
 };
 
-const mockDatas = Array.from({ length: 9 }, () => mockData);
+const mockDatas = Array.from({ length: 7 }, () => mockData);
 
 function CardList() {
-  const slideBar = useRef();
+  const [containerWidth, setContainerWidth] = useState();
+  const [carouselLength, setCarouselLength] = useState();
   const [slidePosition, setSlidePosition] = useState(0);
+  const [isDragging, setIsDragging] = useState(false);
+  const [startTouchPosition, setStartTouchPosition] = useState(0);
 
+  const container = useRef();
+  const slideBar = useRef();
+  const card = useRef();
+
+  /***************** PC 사이즈에서 화살표 클릭으로 캐러셀 제어 *****************/
   const handleLeftClick = () => {
     const slideNode = slideBar.current;
     setSlidePosition(slidePosition + 118);
@@ -33,19 +41,80 @@ function CardList() {
     setSlidePosition(slidePosition - 118);
     slideNode.style.transform = `translate(${slidePosition - 118}rem)`;
   };
+  /********************************************************************/
+
+  /************** 태블릿, 모바일 사이즈에서 터치를 통해 캐러셀 제어 **************/
+  function handleTouchStart(event) {
+    setIsDragging(true);
+    setStartTouchPosition(event.changedTouches[0].clientX);
+  }
+
+  function handleTouchMove(event) {
+    if (!isDragging) return;
+    if (carouselLength <= containerWidth) return;
+
+    const slideNode = slideBar.current;
+    const currentTouchPosition = event.touches[0].clientX;
+    const difference = startTouchPosition - currentTouchPosition;
+
+    slideNode.style.transform = `translate(${slidePosition - difference / 10}rem)`;
+    setSlidePosition(slidePosition - difference / 10);
+    setStartTouchPosition(currentTouchPosition);
+  }
+
+  function handleTouchEnd() {
+    setIsDragging(false);
+
+    if (carouselLength <= containerWidth) return;
+
+    /* 캐러셀 재위치 */
+    const slideNode = slideBar.current;
+
+    if (slidePosition > 0) {
+      slideNode.style.transform = `translate(0)`;
+      setSlidePosition(0);
+      return;
+    }
+
+    if (slidePosition < (containerWidth - carouselLength) / 10) {
+      slideNode.style.transform = `translate(${(containerWidth - carouselLength) / 10}rem)`;
+      setSlidePosition((containerWidth - carouselLength) / 10);
+      return;
+    }
+    /*************/
+  }
+  /******************************************************************/
+
+  useEffect(() => {
+    setContainerWidth(container?.current?.offsetWidth);
+    setCarouselLength(
+      card?.current?.offsetWidth === 208
+        ? 220 * mockDatas?.length - 12
+        : 295 * mockDatas?.length - 20,
+    );
+  }, []);
 
   return (
-    <StyledContainer layout="outer">
-      <StyledContainer>
+    <StyledContainer $layout="outer">
+      <StyledContainer
+        ref={container}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+      >
         <StyledSlideBar ref={slideBar}>
           {mockDatas.map((mockData, index) => {
-            return <Card key={index + 1} data={mockData} />;
+            return (
+              <div key={index + 1} ref={card}>
+                <Card data={mockData} />
+              </div>
+            );
           })}
         </StyledSlideBar>
         {slidePosition < 0 && (
           <ArrowButton
             onClick={handleLeftClick}
-            position="left"
+            $position="left"
             src={arrowImg}
             alt="previous"
           />
@@ -53,7 +122,7 @@ function CardList() {
         {slidePosition > -(29.5 * (mockDatas?.length - 4) - 2) && (
           <ArrowButton
             onClick={handleRightClick}
-            position="right"
+            $position="right"
             src={arrowImg}
             alt="next"
           />
@@ -65,24 +134,34 @@ function CardList() {
 
 const StyledContainer = styled.div`
   max-width: 116rem;
-  position: ${({ layout }) => (layout === 'outer' ? 'relative' : '')};
-  overflow: ${({ layout }) => (layout === 'outer' ? '' : 'hidden')};
+  position: ${({ $layout }) => ($layout === 'outer' ? 'relative' : '')};
+  overflow: ${({ $layout }) => ($layout === 'outer' ? '' : 'hidden')};
 `;
 
 const StyledSlideBar = styled.div`
   display: flex;
-  gap: 2rem;
+  gap: 1.2rem;
   transition: transform 0.5s;
+
+  @media (min-width: 48rem) {
+    gap: 2rem;
+  }
 `;
 
 const ArrowButton = styled.img`
+  display: none;
   width: 4rem;
-  transform: ${({ position }) => (position === 'left' ? 'rotate(180deg)' : '')};
+  transform: ${({ $position }) =>
+    $position === 'left' ? 'rotate(180deg)' : ''};
   position: absolute;
-  left: ${({ position }) => (position === 'left' ? -2 : '')}rem;
-  right: ${({ position }) => (position === 'right' ? -2 : '')}rem;
+  left: ${({ $position }) => ($position === 'left' ? -2 : '')}rem;
+  right: ${({ $position }) => ($position === 'right' ? -2 : '')}rem;
   top: 42.3%;
   cursor: pointer;
+
+  @media (min-width: 64rem) {
+    display: block;
+  }
 `;
 
 export default CardList;
