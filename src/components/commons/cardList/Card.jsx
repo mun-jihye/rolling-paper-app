dd67f2a4908cb5d22bc4f6169d379a5e77945cc4
Jin-Chanyong <cksdyd324@gmail.com>
2024-02-29 00:15:18 +0900

    [#6] feat: 태블릿, 모바일 사이즈 드래그 캐러셀 기능 추가

--- src/components/commons/cardList/Card.jsx
+++ src/components/commons/cardList/Card.jsx
@@ -6,76 +6,15 @@ import orangePattern from 'components/assets/images/cardList/pattern_orange.png'
 import purplePattern from 'components/assets/images/cardList/pattern_purple.png';
 
 function Card({ data }) {
-  const StyledCard = styled.div`
-    padding: 3rem 2.2rem 2rem 2.4rem;
-    flex-shrink: 0;
-    width: 20.8rem;
-    height: 23.2rem;
-    border-radius: 1.6rem;
-    border: 0.1rem solid rgba(0, 0, 0, 0.1);
-    background: ${({ theme }) =>
-      data?.imageSource
-        ? `linear-gradient(180deg, rgba(0, 0, 0, 0.54) 0%, rgba(0, 0, 0, 0.54) 100%), url(${data?.imageSource})`
-        : theme[BACK_GROUND[data?.color][0]]};
-    background-image: ${data?.imageSource
-      ? ''
-      : `url(${BACK_GROUND[data?.color][1]})`};
-    background-position: ${data?.imageSource ? 'center' : 'right bottom'};
-    background-repeat: no-repeat;
-    background-size: ${data?.imageSource ? 'cover' : '10.74rem'};
-    box-shadow: 0 0.2rem 1.2rem 0 rgba(0, 0, 0, 0.08);
-    cursor: pointer;
-
-    @media (min-width: 48rem) {
-      padding: 3rem 2.4rem 2rem;
-      width: 27.5rem;
-      height: 26rem;
-      background-size: ${data?.imageSource ? 'cover' : '14.2rem'};
-    }
-  `;
-
-  const StyledH3tag = styled.h3`
-    overflow: hidden;
-    color: ${({ theme }) => (data?.imageSource ? theme.white : theme.gray900)};
-    text-overflow: ellipsis;
-    font-size: 1.8rem;
-    font-style: normal;
-    font-weight: 700;
-    line-height: 2.8rem;
-    letter-spacing: -0.018rem;
-
-    @media (min-width: 48rem) {
-      font-size: 2.4rem;
-      line-height: 3.6rem;
-      letter-spacing: -0.024rem;
-    }
-  `;
-
-  const StyledPtag = styled.p`
-    display: inline-block;
-    color: ${({ theme }) => (data?.imageSource ? theme.white : theme.gray700)};
-    font-size: 1.4rem;
-    font-style: normal;
-    font-weight: ${({ isNumber }) => (isNumber ? '700' : '400')};
-    line-height: 2rem;
-    letter-spacing: -0.007rem;
-
-    @media (min-width: 48rem) {
-      font-size: 1.6rem;
-      line-height: 2.6rem;
-      letter-spacing: -0.016rem;
-    }
-  `;
-
   return (
-    <StyledCard>
-      <StyledContainer isProfile={true}>
-        <StyledH3tag>{`To. ${data?.name}`}</StyledH3tag>
-        <StyledContainer isImage={true}>
+    <StyledCard $data={data}>
+      <StyledContainer $isProfile={true}>
+        <StyledH3tag $data={data}>{`To. ${data?.name}`}</StyledH3tag>
+        <StyledContainer $isImage={true}>
           {data?.profiles?.slice(0, 3).map((profile, index) => {
             return (
-              <ProfileContainer key={index + 1} index={index}>
-                <Profile src={profile?.imageSource} isModal={false} />
+              <ProfileContainer key={index + 1} $index={index}>
+                <Profile src={profile?.imageSource} $isModal={false} />
               </ProfileContainer>
             );
           })}
@@ -83,16 +22,18 @@ function Card({ data }) {
             <LastProfile>+{data?.profiles?.length - 3}</LastProfile>
           )}
         </StyledContainer>
-        <StyledPtag>
-          <StyledPtag isNumber={true}>{data?.profiles?.length}</StyledPtag>명이
-          작성했어요!
-        </StyledPtag>
+        <StyledText $data={data}>
+          <StyledText $data={data} $isNumber={true}>
+            {data?.profiles?.length}
+          </StyledText>
+          명이 작성했어요!
+        </StyledText>
       </StyledContainer>
       <StyledHrtag />
-      <StyledContainer isBadge={true}>
-        <div></div>
-        <div></div>
-        <div></div>
+      <StyledContainer $isBadge={true}>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
       </StyledContainer>
     </StyledCard>
   );
@@ -105,24 +46,89 @@ const BACK_GROUND = {
   green: ['green200', greenPattern],
 };
 
+const StyledCard = styled.div`
+  padding: 3rem 2.2rem 2rem 2.4rem;
+  flex-shrink: 0;
+  width: 20.8rem;
+  height: 23.2rem;
+  border-radius: 1.6rem;
+  border: 0.1rem solid rgba(0, 0, 0, 0.1);
+  background: ${({ theme, $data }) =>
+    $data?.imageSource
+      ? `linear-gradient(180deg, rgba(0, 0, 0, 0.54) 0%, rgba(0, 0, 0, 0.54) 100%), url(${$data?.imageSource})`
+      : theme[BACK_GROUND[$data?.color][0]]};
+  background-image: ${({ $data }) =>
+    $data?.imageSource ? '' : `url(${BACK_GROUND[$data?.color][1]})`};
+  background-position: ${({ $data }) =>
+    $data?.imageSource ? 'center' : 'right bottom'};
+  background-repeat: no-repeat;
+  background-size: ${({ $data }) =>
+    $data?.imageSource ? 'cover' : '10.74rem'};
+  box-shadow: 0 0.2rem 1.2rem 0 rgba(0, 0, 0, 0.08);
+  cursor: pointer;
+
+  @media (min-width: 48rem) {
+    padding: 3rem 2.4rem 2rem;
+    width: 27.5rem;
+    height: 26rem;
+    background-size: ${({ $data }) =>
+      $data?.imageSource ? 'cover' : '14.2rem'};
+  }
+`;
+
+const StyledH3tag = styled.h3`
+  overflow: hidden;
+  color: ${({ theme, $data }) =>
+    $data?.imageSource ? theme.white : theme.gray900};
+  text-overflow: ellipsis;
+  font-size: 1.8rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 2.8rem;
+  letter-spacing: -0.018rem;
+
+  @media (min-width: 48rem) {
+    font-size: 2.4rem;
+    line-height: 3.6rem;
+    letter-spacing: -0.024rem;
+  }
+`;
+
+const StyledText = styled.div`
+  display: inline-block;
+  color: ${({ theme, $data }) =>
+    $data?.imageSource ? theme.white : theme.gray700};
+  font-size: 1.4rem;
+  font-style: normal;
+  font-weight: ${({ $isNumber }) => ($isNumber ? '700' : '400')};
+  line-height: 2rem;
+  letter-spacing: -0.007rem;
+
+  @media (min-width: 48rem) {
+    font-size: 1.6rem;
+    line-height: 2.6rem;
+    letter-spacing: -0.016rem;
+  }
+`;
+
 const StyledContainer = styled.div`
   display: flex;
-  flex-direction: ${({ isProfile }) => (isProfile ? 'column' : 'row')};
-  gap: ${({ isBadge, isImage }) =>
-    isBadge ? '0.4rem' : isImage ? 0 : '1.2rem'};
+  flex-direction: ${({ $isProfile }) => ($isProfile ? 'column' : 'row')};
+  gap: ${({ $isBadge, $isImage }) =>
+    $isBadge ? '0.4rem' : $isImage ? 0 : '1.2rem'};
 
   & div {
-    height: ${({ isBadge, isImage }) =>
-      isBadge ? '3.2rem' : isImage && '2.8rem'};
+    height: ${({ $isBadge, $isImage }) =>
+      $isBadge ? '3.2rem' : $isImage && '2.8rem'};
   }
 
   @media (min-width: 48rem) {
-    gap: ${({ isBadge, isImage }) =>
-      isBadge ? '0.8rem' : isImage ? 0 : '1.2rem'};
+    gap: ${({ $isBadge, $isImage }) =>
+      $isBadge ? '0.8rem' : $isImage ? 0 : '1.2rem'};
 
     & div {
-      height: ${({ isBadge, isImage }) =>
-        isBadge ? '3.6rem' : isImage && '2.8rem'};
+      height: ${({ $isBadge, $isImage }) =>
+        $isBadge ? '3.6rem' : $isImage && '2.8rem'};
     }
   }
 `;
@@ -141,7 +147,7 @@ const StyledHrtag = styled.hr`
 
 const ProfileContainer = styled.div`
   position: relative;
-  left: ${({ index }) => index * -1.2}rem;
+  left: ${({ $index }) => $index * -1.2}rem;
 `;
 
 const LastProfile = styled.div`
