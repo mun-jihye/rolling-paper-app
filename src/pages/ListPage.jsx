08acc981939cd0fc2420df6fa67ecf0b9573eec7
Jin-Chanyong <cksdyd324@gmail.com>
2024-03-01 14:03:43 +0900

    [#30] feat: ListPage UI 구현 완료, Card 컴포넌트에 Badge 추가, useDeviceType 커스텀 훅 생성

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -3,20 +3,30 @@ import MainHeader from 'components/commons/header/MainHeader';
 import CardList from 'components/commons/cardList/CardList';
 import styled from 'styled-components';
 import PrimaryBtn from 'components/commons/buttons/PrimaryBtn';
+import useDeviceType from 'hooks/useDeviceType';
 
 const ListPage = () => {
+  const deviceType = useDeviceType();
   return (
     <div>
       <MainHeader />
       <PageContainer>
-        <section>
+        <SectionConainer>
           <StyledHtag>인기 롤링 페이퍼 🔥</StyledHtag>
-          <CardList />
-        </section>
-        <section>
+          <CardList
+            carouselMargin={
+              deviceType === 'PC' ? 0 : deviceType === 'Tablet' ? 2.4 : 2
+            }
+          />
+        </SectionConainer>
+        <SectionConainer>
           <StyledHtag>최근에 만든 롤링 페이퍼⭐️</StyledHtag>
-          <CardList />
-        </section>
+          <CardList
+            carouselMargin={
+              deviceType === 'PC' ? 0 : deviceType === 'Tablet' ? 2.4 : 2
+            }
+          />
+        </SectionConainer>
         <StyledFooter>
           <ListPagePrimaryBtn>나도 만들어보기</ListPagePrimaryBtn>
         </StyledFooter>
@@ -39,8 +49,15 @@ const PageContainer = styled.main`
     padding-top: 5rem;
     padding-bottom: 13.2rem;
   }
+
   @media (min-width: 75rem) {
     padding-bottom: 4rem;
+  }
+`;
+
+const SectionConainer = styled.section`
+  @media (min-width: 75rem) {
+    margin: 0 auto;
   }
 `;
 
@@ -50,6 +67,7 @@ const StyledHtag = styled.h1`
   font-weight: 600;
   line-height: 3rem;
   margin-bottom: 1.2rem;
+  margin-left: 2rem;
 
   @media (min-width: 48rem) {
     font-size: 2.4rem;
@@ -57,6 +75,11 @@ const StyledHtag = styled.h1`
     line-height: 3.6rem;
     letter-spacing: -0.024rem;
     margin-bottom: 1.6rem;
+    margin-left: 2.4rem;
+  }
+
+  @media (min-width: 75rem) {
+    margin-left: 0;
   }
 `;
 
@@ -75,10 +98,17 @@ const StyledFooter = styled.footer`
 
 const ListPagePrimaryBtn = styled(PrimaryBtn)`
   width: 100%;
+  max-widht: 116rem;
+  transition: transform 0.5s;
 
   @media (min-width: 75rem) {
     width: 28rem;
+    height: 5.6rem;
     display: block;
     margin: 0 auto;
+    font-size: 1.8rem;
+    font-weight: 700;
+    line-height: 2.8rem;
+    letter-spacing: -0.018rem;
   }
 `;
