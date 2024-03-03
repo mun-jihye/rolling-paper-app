ee01cc9a4e1003b3cd8378fda29f9e55d9c9c11c
KimChangmin <zzangmin3@gmail.com>
2024-03-03 13:45:14 +0900

    [#42] feat: mk postPage & Options.jsx & edit TextfieldInput & edit ToggleBtn

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,11 +1,73 @@
-import { React } from 'react';
+import { React, useState, useMemo, useEffect } from 'react';
 import styled from 'styled-components';
 import GNB from 'components/commons/header/GNB';
-import { TextFieldInput } from 'components/commons/form';
+import { TextFieldInput, Options } from 'components/commons/form';
+import ToggleBtn from 'components/commons/buttons/ToggleBtn';
+import Primary from 'components/commons/buttons/PrimaryBtn';
+import nation1 from 'assets/backgrounds/nation1.svg';
+import nation2 from 'assets/backgrounds/nation2.svg';
 
 const PostPage = () => {
-  const disabled = false;
-  const error = {};
+  const [toggleState, setToggleState] = useState('컬러');
+  const items = useMemo(() => {
+    return toggleState === '컬러'
+      ? ['orange200', 'purple200', 'blue200', 'green200']
+      : [nation1, nation2, nation1, nation2];
+  }, [toggleState]);
+
+  const [selectedItem, setSelectedItem] = useState(items[0]);
+  const [isBtnDisabled, setIsBtnDisabled] = useState(true);
+  const [formValues, setFormValues] = useState({
+    textFieldInput: '',
+    selectedItem,
+  });
+  const inputDisabled = false;
+
+  const error = { message: '' };
+
+  const handleChange = e => {
+    if (!e.target.value) {
+      setIsBtnDisabled(true);
+    } else {
+      setIsBtnDisabled(false);
+    }
+    setFormValues({
+      ...formValues,
+      textFieldInput: e.target.value,
+    });
+  };
+
+  const handleToggle = e => {
+    console.log('toggle');
+    setToggleState(e.target.innerText.toLowerCase());
+    setSelectedItem(items[0]);
+    setFormValues(prevState => ({
+      ...prevState,
+      selectedItem: items[0],
+    }));
+  };
+  const handleSelect = selectedItem => {
+    setFormValues(prevState => ({
+      ...prevState,
+      selectedItem: items[0],
+    }));
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(formValues);
+    // this.props.history.push({
+    //   pathname: '/post/{id}',
+    //   state: { formValues },
+    // });
+  };
+
+  useEffect(() => {
+    setFormValues(prevState => ({
+      ...prevState,
+      selectedItem,
+    }));
+  }, [selectedItem]);
 
   return (
     <>
@@ -13,21 +75,34 @@ const PostPage = () => {
       <AddPostForm className="post-container">
         <InputContainer>
           <h2>To</h2>
-          <TextFieldInput
+          <StyledTextFieldInput
             placeholder={'받는 사람 이름을 입력해주세요'}
-            disabled={disabled}
+            disabled={inputDisabled}
             error={error}
+            handleChange={handleChange}
           />
         </InputContainer>
         <SelectContainer>
           <Description>
-            <h2>배경화면을 선택해주세요</h2>
-            <p>컬러를 성택하거나, 이미지를 선택할 수 있습니다.</p>
+            <h2>배경화면을 선택해 주세요.</h2>
+            <p>컬러를 선택하거나, 이미지를 선택할 수 있습니다.</p>
           </Description>
-          <SelectButton />
-          <PreviewBlocks />
+          <ToggleBtn onClick={handleToggle} />
+          <StyledOptions
+            toggleState={toggleState}
+            selectedItem={selectedItem}
+            setSelectedItem={setSelectedItem}
+            handleSelect={handleSelect}
+            items={items}
+          />
         </SelectContainer>
-        <CreateButton />
+        <StyledBtn
+          type="submit"
+          onClick={handleSubmit}
+          disabled={isBtnDisabled}
+        >
+          생성하기
+        </StyledBtn>
       </AddPostForm>
     </>
   );
@@ -43,7 +118,6 @@ const AddPostForm = styled.div`
   inset: 0;
   padding: 5.7rem 0 35rem;
 
-  background-color: gray;
   z-index: 1;
 `;
 
@@ -51,10 +125,8 @@ const InputContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  background-color: green;
-  height: 100px;
 
-  & > h2 {
+  h2 {
     font-size: 2.4rem;
     font-weight: 700;
     line-height: 4.2rem;
@@ -63,27 +135,47 @@ const InputContainer = styled.div`
   }
 `;
 
+const StyledTextFieldInput = styled(TextFieldInput)`
+  width: 72rem;
+`;
+
 const SelectContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-end;
+  align-items: flex-start;
 
   position: relative;
   padding-top: 5rem;
+  gap: 0.4rem;
 `;
 const Description = styled.div`
-  & > h2 {
+  margin-bottom: 2.4rem;
+  h2 {
     font-size: 2.4rem;
     font-weight: 700;
     line-height: 4.2rem;
     letter-spacing: -0.01em;
+    margin-bottom: 0.4rem;
+  }
+  p {
+    font-size: 1.6rem;
+    line-height: 2.6rem;
+    letter-spacing: -0.01em;
+    text-align: left;
+
+    color: ${({ theme }) => theme.gray500};
   }
 `;
-const SelectButton = styled.div``;
-const PreviewBlocks = styled.div`
-  position: relative;
-  display: flex;
+
+const StyledOptions = styled(Options)`
+  padding: 4.5rem 0;
 `;
-const CreateButton = styled.button``;
+const StyledBtn = styled(Primary)`
+  width: 72rem;
+
+  font-size: 1.8rem;
+  line-height: 2.8rem;
+  letter-spacing: -0.01em;
+`;
 
 export default PostPage;
