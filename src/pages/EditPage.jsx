fd87d102485862962a09e10e4f5750589372545c
mun-jihye <m84422583@gmail.com>
2024-03-04 22:12:48 +0900

    fix: conflict 해결

--- src/pages/EditPage.jsx
+++ src/pages/EditPage.jsx
@@ -2,15 +2,25 @@ import React from 'react';
 import { EditContainer } from 'components/edit/EditContainer';
 import FromCardList from 'components/edit/FromCardList';
 import { editData } from 'data/editPage';
+import { useQuery } from 'react-query';
+import { getRecipientList } from 'api/recipient';
 
-const datas = editData.recentMessages;
+// const datas = editData.recentMessages;
 const EditPage = () => {
+  const { data, error } = useQuery({
+    queryKey: ['editPage'],
+    queryFn: ({ id = 4080 }) => getRecipientList(id),
+  });
+  const editData = data?.data.results[0];
+  console.log(data);
+  console.log(error);
+
   return (
     <EditContainer
-      backgroundColor={editData.backgroundColor}
-      backgroundImageURL={editData.backgroundImageURL}
+    // $backgroundcolor={editData.backgroundColor}
+    // $backgroundimageurl={editData.backgroundImageURL}
     >
-      <FromCardList datas={datas} />
+      {/* <FromCardList datas={datas} /> */}
     </EditContainer>
   );
 };
--- src/utils/constants/API.js
+++ src/utils/constants/API.js
@@ -1,6 +1,6 @@
 export const AUTH = Object.freeze({
   backgroundImages: 'background-images/',
   profileImages: 'profile-images/',
-  message: '4-24/message/',
+  messages: '4-24/message/',
   recipients: '4-24/recipients/',
 });
