881d8f124827d7cb51ab7a7dc6a173ebe2d37d80
mun-jihye <m84422583@gmail.com>
2024-03-05 17:49:32 +0900

    [#66] design: 편집하기 버튼 디자인 구현

--- src/pages/EditPage.jsx
+++ src/pages/EditPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { EditContainer } from 'components/edit/EditContainer';
 import FromCardList from 'components/edit/FromCardList';
 import { useQuery } from 'react-query';
@@ -7,10 +7,13 @@ import Loader from 'components/commons/Loader';
 import MainHeader from 'components/commons/header/MainHeader';
 import SubHeader from 'components/commons/header/SubHeader';
 import { useParams } from 'react-router-dom';
+import Button from 'components/commons/buttons/Button';
+import styled from 'styled-components';
 
 const EditPage = () => {
   const { postId } = useParams();
-  console.log(postId);
+  const [isEdit, setIsEdit] = useState(true);
+
   const { data, isLoading } = useQuery({
     queryKey: ['editPage', postId],
     queryFn: context => getRecipient(context.queryKey[1]),
@@ -36,11 +39,38 @@ const EditPage = () => {
           $backgroundcolor={editData.backgroundColor}
           $backgroundimageurl={editData.backgroundImageURL}
         >
+          <FlexContainer>
+            {isEdit ? (
+              <StyledButton>편집하기</StyledButton>
+            ) : (
+              <StyledButton>삭제하기</StyledButton>
+            )}
+          </FlexContainer>
           <FromCardList datas={messageData} />
         </EditContainer>
       )}
     </>
   );
 };
-
+const FlexContainer = styled.div`
+  width: 100%;
+  max-width: 120rem;
+  margin: 0 auto;
+  position: relative;
+`;
+const StyledButton = styled(Button)`
+  position: absolute;
+  bottom: 1rem;
+  right: 0;
+  @media ${({ theme }) => theme.breakpoint.tablet} {
+    position: fixed;
+    bottom: 0;
+    right: auto;
+  }
+  @media ${({ theme }) => theme.breakpoint.mobile} {
+    position: fixed;
+    bottom: 0;
+    right: auto;
+  }
+`;
 export default EditPage;
