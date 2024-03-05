b2564039ac75541f0a3d3118feb5ebc720b5bb63
yubin <yb3143@gmail.com>
2024-03-05 16:35:43 +0900

    [#77] feat: ErrorPage 파일 생성

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import GNB from 'components/commons/header/GNB';
+import React from 'react';
+
+const ErrorPage = () => {
+  return (
+    <>
+      <GNB />
+      <div>ErrorPage</div>
+    </>
+  );
+};
+
+export default ErrorPage;
