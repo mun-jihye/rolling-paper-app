567474f60d59a504cf40a02d2667e7635c9f771b
Jin-Chanyong <cksdyd324@gmail.com>
2024-03-06 19:33:18 +0900

    [#99] feat: mockData 추가

--- src/pages/PapersPage.jsx
+++ src/pages/PapersPage.jsx
@@ -1,5 +1,36 @@
+import Card from 'components/commons/cardList/Card';
+import GNB from 'components/commons/header/GNB';
+import mockDatas from 'data/papersPage';
+import styled from 'styled-components';
+
 const PapersPage = () => {
-  return;
+  return (
+    <div>
+      <GNB />
+      <GridContainer>
+        {mockDatas?.map(mockData => {
+          return <Card key={mockData.id} data={mockData} />;
+        })}
+      </GridContainer>
+    </div>
+  );
 };
 
 export default PapersPage;
+
+const GridContainer = styled.div`
+  width: 100%;
+  max-width: 120rem;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+  justify-items: center;
+  align-items: center;
+  @media ${({ theme }) => theme.breakpoint.tablet} {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media ${({ theme }) => theme.breakpoint.mobile} {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
