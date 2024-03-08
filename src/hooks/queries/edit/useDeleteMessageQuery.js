7c71669ce9c774022df3ab8e4a75054d4831b800
mun-jihye <m84422583@gmail.com>
2024-03-08 16:49:44 +0900

    [#117] chore: api 함수 jsdoc 추가

--- src/hooks/queries/edit/useDeleteMessageQuery.js
+++ src/hooks/queries/edit/useDeleteMessageQuery.js
@@ -2,6 +2,11 @@ import { deleteMessage } from 'api/message';
 import { useMutation, useQueryClient } from 'react-query';
 import { errorAlert } from 'utils/errorAlert';
 
+/**
+ * 메세지 삭제 요청 쿼리
+ * @param {Number} messageId 삭제 요청 보낼 때 사용할 메세지 아이디
+ * @returns
+ */
 export const useDeleteMessageQuery = messageId => {
   const queryClient = useQueryClient();
   const deleteData = useMutation({
--- src/hooks/queries/edit/useDeleteRecipientQuery.js
+++ src/hooks/queries/edit/useDeleteRecipientQuery.js
@@ -2,6 +2,11 @@ import { deleteRecipient } from 'api/recipient';
 import { useMutation, useQueryClient } from 'react-query';
 import { errorAlert } from 'utils/errorAlert';
 
+/**
+ * 롤링페이퍼 대상 삭제 쿼리
+ * @param {Number} postId 롤링페이퍼 대상 id
+ * @returns
+ */
 export const useDeleteRecipientQuery = postId => {
   const queryClient = useQueryClient();
   const deleteData = useMutation({
